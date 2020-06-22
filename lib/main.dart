4c9032ed2ca1be27e142346038b1e26d40b115f5
Hung Pham <hungps.work@gmail.com>
2020-06-23 01:09:21 +0700

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_appbar/sample_data.dart';
 import 'package:gradient_appbar/widgets/color_item.dart';
@@ -26,26 +27,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const double _endReachedThreshold = 200;
   static const int _itemsPerPage = 20;
+
+  final ScrollController _controller = ScrollController();
 
   List<ColorInformation> _colors = [];
   int _nextPage = 1;
+  bool _loading = true;
+  bool _canLoadMore = true;
 
   @override
   void initState() {
+    _controller.addListener(_onScroll);
+
     _getColors();
 
     super.initState();
   }
 
   Future<void> _getColors() async {
+    _loading = true;
+
     final newColors = await getColorsFromServer(page: _nextPage, limit: _itemsPerPage);
 
     setState(() {
       _colors.addAll(newColors);
 
       _nextPage++;
+
+      if (newColors.length < _itemsPerPage) {
+        _canLoadMore = false;
+      }
+
+      _loading = false;
     });
+  }
+
+  void _onScroll() {
+    if (!_controller.hasClients || _loading) return;
+
+    final thresholdReached = _controller.position.extentAfter < _endReachedThreshold;
+
+    if (thresholdReached) {
+      _getColors();
+    }
+  }
+
+  Future<void> _refresh() async {
+    _canLoadMore = true;
+    _colors.clear();
+    _nextPage = 1;
+    await _getColors();
   }
 
   Widget _buildColorItem(BuildContext context, int index) {
@@ -61,16 +94,37 @@ class _MyHomePageState extends State<MyHomePage> {
           borderRadius: BorderRadius.circular(30),
         ),
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(16),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          childAspectRatio: 1.6,
-          crossAxisCount: 2,
-          crossAxisSpacing: 16,
-          mainAxisSpacing: 16,
-        ),
-        itemCount: _colors.length,
-        itemBuilder: _buildColorItem,
+      body: CustomScrollView(
+        controller: _controller,
+        slivers: <Widget>[
+          CupertinoSliverRefreshControl(
+            onRefresh: _refresh,
+          ),
+          SliverPadding(
+            padding: EdgeInsets.all(16),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 1.6,
+                crossAxisCount: 2,
+                crossAxisSpacing: 3,
+                mainAxisSpacing: 16,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                _buildColorItem,
+                childCount: _colors.length,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: _canLoadMore
+                ? Container(
+                    padding: EdgeInsets.only(bottom: 16),
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(),
+                  )
+                : SizedBox(),
+          ),
+        ],
       ),
     );
   }
